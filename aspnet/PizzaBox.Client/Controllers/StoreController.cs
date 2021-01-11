4ad831529409effde71d584764c7ccb7d0c436fb
isaiah <isotodev@gmail.com>
2021-01-11 13:08:55 -0600

    All Repos are now attached to Unit of Work Class

--- aspnet/PizzaBox.Client/Controllers/StoreController.cs
+++ aspnet/PizzaBox.Client/Controllers/StoreController.cs
@@ -4,29 +4,25 @@ using PizzaBox.Repo.Repos;
 
 namespace PizzaBox.Client.Controllers
 {
-    [Route("[controller]")]
+    [Route("/")]
     public class StoreController : Controller
     {
       private AllRepo Repo = new AllRepo();
 
       [HttpGet]
-      [Route("/stores")]
       public IActionResult GetListOfStores()
       {
-        //  var stores = new StoreViewModel();
         var stores = Repo.StoreRepo.ReadStores();
-        //1 way-data binding, action to view
-        ViewBag.Stores = stores; //dynamic object;
-        // ViewData["Stores"] = stores.Stores; //dictionary Object
-        // TempData["Stores"] = stores.Stores; //2 way
+        ViewBag.Stores = stores;
         return View("Store");
       }
 
-      // [HttpGet("{store}")]
-      // public IActionResult Get(string Store)
-      // {
-      //   return View("Store", Store);
-      // }
-
+      [Route("/Store/{name}")]
+      public IActionResult GetStoreByName(string name)
+      {
+        var stores = Repo.StoreRepo.ReadOneStore(name);
+        ViewBag.Stores = stores;
+        return View("Store");
+      }
     }
 }
